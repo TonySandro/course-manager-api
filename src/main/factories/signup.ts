2c3580dd5820d6b3eedb5c83fd33851905703a49
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-02 17:35:15 -0300

    refactor: delete duplicated bcrypt files

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,12 +1,11 @@
 import { SignUpController } from "../../presentation/controllers/signup/signup";
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
-import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account-mongo-repository";
 import { Controller } from "../../presentation/protocols";
 import { LogControllerDecorator } from "../decorators/log-controller";
 import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log-mongo-repository";
-import { ValidationComposite } from "../../presentation/helpers/validators/validation-composite";
 import { makeSignUpValidation } from "./signup-validation";
+import { BcryptAdapter } from "./login/login-factory-protocols";
 
 export const makeSignUpController = (): Controller => {
   const addAccountRepository = new AccountMongoRepository();
