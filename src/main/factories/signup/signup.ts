4cfcd659cfec48d714ec6749e42b6ec80ce4e925
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-03 12:48:13 -0300

    refactor: create signup file on controllers

---
+++ src/main/factories/signup/signup.ts
@@ -0,0 +1,21 @@
+import { SignUpController } from "../../../presentation/controllers/signup/signup";
+import { DbAddAccount } from "../../../data/usecases/add-account/db-add-account";
+import { AccountMongoRepository } from "../../../infra/database/mongodb/account-repository/account-mongo-repository";
+import { Controller } from "../../../presentation/protocols";
+import { LogControllerDecorator } from "../../decorators/log-controller";
+import { LogMongoRepository } from "../../../infra/database/mongodb/log-repository/log-mongo-repository";
+import { makeSignUpValidation } from "./signup-validation";
+import { BcryptAdapter } from "../login/login-factory-protocols";
+
+export const makeSignUpController = (): Controller => {
+  const addAccountRepository = new AccountMongoRepository();
+  const bcryptAdapter = new BcryptAdapter(12);
+  const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository);
+  const signUpController = new SignUpController(
+    addAccount,
+    makeSignUpValidation()
+  );
+  const logMongoRepository = new LogMongoRepository();
+
+  return new LogControllerDecorator(signUpController, logMongoRepository);
+};
--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { makeSignUpController } from "../factories/signup";
+import { makeSignUpController } from "../factories/signup/signup";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
 
