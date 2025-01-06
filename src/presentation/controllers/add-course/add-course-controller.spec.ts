91e9f275eaf0ee2701dde7f2cd1f093000739b1e
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-06 17:27:21 -0300

    feat: ensure AddCourseController returns 400 if Validation returns an error

--- src/presentation/controllers/add-course/add-course-controller.spec.ts
+++ src/presentation/controllers/add-course/add-course-controller.spec.ts
@@ -1,14 +1,61 @@
 import { AddCourseController } from "./add-course-controller";
-import { success } from "../../helpers/http/http-helper";
+import { badRequest, success } from "../../helpers/http/http-helper";
+import { HttpRequest, Validation } from "../../protocols";
+import { MissingParamError } from "../../errors";
 
-const makeSut = () => {
-  return new AddCourseController();
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    title: "Advanced JavaScript Course",
+    description: "Learn advanced concepts of JavaScript...",
+    price: 49.99,
+    image_url: "https://example.com/images/advanced-javascript-course.jpg",
+    created_at: new Date("2023-01-01T10:00:00Z"),
+    updated_at: new Date("2023-01-10T12:00:00Z"),
+  },
+});
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null as unknown as Error;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+interface SutTypes {
+  sut: AddCourseController;
+  validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new AddCourseController(validationStub);
+
+  return {
+    sut,
+    validationStub,
+  };
 };
 
 describe("Add Course Controller", () => {
+  test("Should return 400 if Validation returns an error", async () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, "validate")
+      .mockReturnValueOnce(new MissingParamError("any_field"));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError("any_field"))
+    );
+  });
+
   test("Should return 200 if valid data is provided", async () => {
-    const sut = makeSut();
-    const httpResponse = await sut.handle({});
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(success({}));
   });
--- src/presentation/controllers/add-course/add-course-controller.ts
+++ src/presentation/controllers/add-course/add-course-controller.ts
@@ -1,8 +1,20 @@
-import { success } from "../../helpers/http/http-helper";
-import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+import { badRequest, success } from "../../helpers/http/http-helper";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+  Validation,
+} from "../../protocols";
 
 export class AddCourseController implements Controller {
+  constructor(private readonly validation: Validation) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    const error = this.validation.validate(httpRequest.body);
+    if (error) {
+      return badRequest(error);
+    }
+
     return success({});
   }
 }
