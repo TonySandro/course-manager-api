aadf3a0ea2f18107a430e211b95e34cf05d671e9
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-06 17:28:28 -0300

    test: ensure AddCourseController calls Validation with correct value

--- src/presentation/controllers/add-course/add-course-controller.spec.ts
+++ src/presentation/controllers/add-course/add-course-controller.spec.ts
@@ -40,6 +40,15 @@ const makeSut = (): SutTypes => {
 };
 
 describe("Add Course Controller", () => {
+  test("Should call Validation with correct value", async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, "validate");
+    const httpRequest = makeFakeRequest();
+    await sut.handle(httpRequest);
+
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
   test("Should return 400 if Validation returns an error", async () => {
     const { sut, validationStub } = makeSut();
     jest
