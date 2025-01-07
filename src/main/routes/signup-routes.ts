3762e0d06996834bc9c8f57e57ea901f625c8451
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-07 18:48:07 -0300

    feat: add add course route

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { makeSignUpController } from "../factories/signup/signup";
 import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
+import { makeAddCourseController } from "../factories/add-course/add-course";
 
 export default (router: Router): void => {
   router.post("/signup", adaptRoute(makeSignUpController()));
-
   router.post("/login", adaptRoute(makeLoginController()));
+  router.post("/add-course", adaptRoute(makeAddCourseController()));
 };
