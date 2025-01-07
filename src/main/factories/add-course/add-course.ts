13c3fb2ee2be8a9227329f802726267d597202da
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-07 17:57:14 -0300

    feat: add makeAddCourseController factory

---
+++ src/main/factories/add-course/add-course.ts
@@ -0,0 +1,19 @@
+import { Controller } from "../../../presentation/protocols";
+import { LogControllerDecorator } from "../../decorators/log-controller";
+import { LogMysqlRepository } from "../../../infra/database/mysql/log/log-mysql-repository";
+import { AddCourseController } from "../../../presentation/controllers/add-course/add-course-controller";
+import { CourseMysqlRepository } from "../../../infra/database/mysql/course-repository/course-mysql-repository";
+import { DbAddCourse } from "../../../data/usecases/add-course/db-add-course";
+import { makeAddCourseValidation } from "./add-course-validation";
+
+export const makeAddCourseController = (): Controller => {
+  const addCourseRepository = new CourseMysqlRepository();
+  const addCourse = new DbAddCourse(addCourseRepository);
+  const signUpController = new AddCourseController(
+    makeAddCourseValidation(),
+    addCourse
+  );
+  const logMysqlRepository = new LogMysqlRepository();
+
+  return new LogControllerDecorator(signUpController, logMysqlRepository);
+};
