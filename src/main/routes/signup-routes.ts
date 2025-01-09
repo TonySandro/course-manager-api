58dc053b833a9d266d749382c6a9026b9c942f2a
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-01-09 00:04:12 -0300

    feat: adding add-module in routes

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -4,10 +4,14 @@ import { adaptRoute } from "../adapters/express/express-route-adapter";
 import { makeLoginController } from "../factories/login/login-factory";
 import { makeAddCourseController } from "../factories/add-course/add-course";
 import { makeListCourseController } from "../factories/list-course/list-course";
+import { makeAddModuleController } from "../factories/add-module/add-module-factory";
 
 export default (router: Router): void => {
   router.post("/signup", adaptRoute(makeSignUpController()));
   router.post("/login", adaptRoute(makeLoginController()));
+
   router.post("/add-course", adaptRoute(makeAddCourseController()));
   router.get("/list-course", adaptRoute(makeListCourseController()));
+
+  router.post("/add-module", adaptRoute(makeAddModuleController()));
 };
